840ddc5d507bca8789a6abf71265b149ab81b18f
saidtimaev <said.timaev@hotmail.com>
2024-03-04 09:33:30 +0100

    routeur index.php + template.php

---
+++ index.php
@@ -0,0 +1,11 @@
+// Routeur
+
+<?php
+
+require_once('controllers/');
+require_once('controllers/');
+
+if (isset($_GET['action']) && $_GET['action'] !== '') {
+	
+	
+}
---
+++ view/template.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="public/css/styles.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <title><?= $titrePage ?></title>
+    </head>
+    <body >
+        
+            <header >
+                <div class="container containerNav">
+                    <nav class="navbar justify-content-center">
+                        <ul class="navbar-nav d-flex flex-row">
+                                <?= $pageActive ?>
+                        </ul>
+                    </nav>
+                </div>
+                
+            </header>
+            <main>
+            <?= $content ?>
+            </main>
+    </body>
+</html>
