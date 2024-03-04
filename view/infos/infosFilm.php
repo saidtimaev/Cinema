8579f7edc25ad8c9d1c1b02b789113557092e519
saidtimaev <said.timaev@hotmail.com>
2024-03-04 16:34:07 +0100

    maj vue infosFilm

--- view/infos/infosFilm.php
+++ view/infos/infosFilm.php
@@ -1,36 +1,18 @@
 <?php ob_start(); ?>
 <?php $film = $requeteInfosFilm->fetch();  ?>
-<p><?php echo "Infos"; ?></p>
 
-<table>
-    <thead>
-        <tr>
-            <th>Titre</th>
-            <th>Date de sortie</th>
-            <th>Durée</th>
-            <th>Note</th>
-            <th>Résumé</th>
-            <th>Prénom réalisateur</th>
-            <th>Nom réalisateur</th>
-        </tr>
-    </thead>
-    <tbody>
-        
-                <tr>
-                    <td><?php echo $film["film_titre"]; ?></td>
-                    <td><?php echo $film["film_date_sortie"]; ?></td>
-                    <td><?php echo $film["film_duree"]; ?></td>
-                    <td><?php echo $film["film_note"]; ?></td>
-                    <td><?php echo $film["film_synopsis"]; ?></td>
-                    <td><?php echo $film["personne_prenom"]; ?></td>
-                    <td><?php echo $film["personne_nom"]; ?></td>
-                    
-                </tr>
-            
-    </tbody>
-</table>
+<h1><?php echo $film["film_titre"]."<br>"; ?> </h1>
+<p>Date de Sortie : <?php echo $film["film_date_sortie"]; ?></p>
+<p>Durée : <?php echo $film["film_duree"]; ?> min</p>
+<p>Note : <?php echo $film["film_note"]; ?></p>
+<p>Resumé : <br><?php echo $film["film_synopsis"]; ?></p>
+<p>Réalisateur : <?php echo $film["personne_prenom"]." ".$film["personne_nom"]; ?></p>
 
-<p>Liste des rôles et des acteurs</p>
+
+   
+
+
+<p>Liste des rôles et des acteurs :</p>
 
 <table>
     <thead>
@@ -54,6 +36,6 @@
 <?php
 
 $titrePage = $film["film_titre"];
-$titreSecond = $film["film_titre"];
+$titreSecond = "";
 $content = ob_get_clean();
 require "view/template.php";
