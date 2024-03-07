abb901c16517b3afc6e390b376b4a160b64066f0
saidtimaev <said.timaev@hotmail.com>
2024-03-07 09:20:26 +0100

    ajout commentaires

--- model/connect.php
+++ model/connect.php
@@ -13,9 +13,12 @@ abstract class Connect {
 
     // Différence entre PDO et MYSQLi, PDO permet l'accès à 11 BDD's
 
-    
+
     // 4 principes fondamentaux de la programmation orienté objet
-    // L'encapsulation, l'abstraction, l'héritage, le polymorphisme
+    // L'encapsulation, regroupement de données et les différents moyens qui autorisent leur lecture ou manipulation
+    // l'abstraction, 
+    // l'héritage, permet d'inclure dans une classe les caractéristiques d'une autre classe 
+    // le polymorphisme, permet à des classes d'implémenter la même méthode mais avec un comportement différent 
     public static function seConnecter(){
         try {
                 return new \PDO(
